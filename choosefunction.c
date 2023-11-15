e9ea73759b064b66ecaef9be66350b302b956a7a
HadeerAdelouf <hadiradel999@gmail.com>
2023-11-15 14:12:27 -0800

    monty

---
+++ choosefunction.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+/**
+ * choose_function - Select function for needing
+ * @head: Head of the list
+ * @token: Parameters for entry
+ * @counter: Number of the line
+ *
+ * Return: Void - Nothing
+ **/
+void choose_function(stack_t **head, char *token, unsigned int counter)
+{
+	instruction_t selector[] = {
+		{"pall", pall_stack},
+		{"push", push_stack},
+		{"pint", pint},
+	};
+
+	int move = 0;
+
+	while (move < 10)
+	{
+		if (strcmp(selector[move].opcode, token) == 0)
+		{
+			selector[move].f(head, counter);
+			return;
+		}
+		move++;
+	}
+	if (token[0] != '#')
+	{
+		fprintf(stderr, "L%u: unknown instruction %s\n", counter, token);
+		exit(EXIT_FAILURE);
+	}
+}
---
+++ function1.c
@@ -0,0 +1,109 @@
+#include "monty.h"
+/**
+ * free_all - Frees memory in stack
+ * @head: Head of the list
+ * Return: Void - Nothing
+ **/
+
+void free_all(stack_t **head)
+{
+	stack_t *temp;
+	stack_t *h_list;
+
+	h_list = *head;
+
+	while (h_list)
+	{
+		temp = h_list;
+		h_list = h_list->next;
+		free(temp);
+	}
+}
+
+/**
+ * push_stack - Function for push an element to the stack.
+ * @head: Head of the list
+ * @counter: Number of the line
+ * Return: void - nothing
+ **/
+
+void push_stack(stack_t **head, unsigned int counter)
+{
+	stack_t *new_node = NULL;
+	int cont = 0;
+	char *checker = strtok(NULL, LIMIT);
+
+	if (!checker)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", counter);
+		exit(EXIT_FAILURE);
+	}
+
+	while (checker[cont] != '\0')
+	{
+		if (!isdigit(checker[cont]) && checker[cont] != '-')
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", counter);
+			exit(EXIT_FAILURE);
+		}
+		cont++;
+	}
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = atoi(checker);
+	new_node->prev = NULL;
+
+	if (*head)
+	{
+		new_node->next = (*head);
+		(*head)->prev = new_node;
+		(*head) = new_node;
+	}
+	else
+	{
+		(*head) = new_node;
+		new_node->next = NULL;
+	}
+}
+
+/**
+ * pall_stack - Prints all the values on the stack,
+ * starting from the top of the stack.
+ * @head: Head of the list
+ * @counter: Number of the line
+ * Return: void
+ **/
+
+void pall_stack(stack_t **head, unsigned int counter __attribute__((unused)))
+{
+	stack_t *temp = *head;
+
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
+
+/**
+ * pint - prints the value at the top of the stack,
+ * followed by a new line.
+ * @head: Head of the list
+ * @counter: Number of the line
+ * Return: void
+ **/
+
+void pint(stack_t **head, unsigned int counter)
+{
+	if (head == NULL || *head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
